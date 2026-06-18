4a5750931efcf72cdf8287aa12b15b678b3be544
lella amaravani <lellaamaravani@gmail.com>
2026-06-18 12:34:32 +0530

    Update CodeAlpha_Email Extractor.py

--- CodeAlpha_Email Extractor.py
+++ CodeAlpha_Email Extractor.py
@@ -10,16 +10,4 @@ hello123@gmail.com
 emails = re.findall(r'\S+@\S+', text)
 
 for email in emails:
-    print(email)import re
-
-text = """
-Contact us:
-abc@gmail.com
-test@yahoo.com
-hello123@gmail.com
-"""
-
-emails = re.findall(r'\S+@\S+', text)
-
-for email in emails:
-    print(ema
+    print(email)
